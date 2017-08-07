00ab76d96bc4736069322f5f1c254041716a995c
Angello Rodriguez Gutierrez <angellomarcelo.rodriguez.contractor@bbvadata.com>
2017-08-07 09:46:45 +0200

    clean repo

--- Ej5.py
+++
@@ -1,5 +0,0 @@
-from Ej3 import *
-
-em = [20, 18000000, 5000000]
-var = micro_empresa(em)
-
--- Ej6.py
+++
@@ -1,8 +0,0 @@
-
-def transf(temp):
-    tf = 9 * temp / 5 + 32
-    return tf
-
-t = 60
-
-print transf(60)
